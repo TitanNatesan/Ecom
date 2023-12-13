5ac5991f62005b9a6c543a78c6010d79ad984ba2
Titan-Natesan <natesantitan@gmail.com>
2023-12-13 11:12:49 +0530

    app added to settings

--- Ecom_B/Ecom_B/settings.py
+++ Ecom_B/Ecom_B/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-js8+$pktobvb5s(q^pbiimb@paywn_vis^)2%fp+oq-&7)d(@3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application  definition
