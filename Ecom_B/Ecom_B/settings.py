d02dd71ea23757de55f2cc9bb179e2a8208adab5
Titan-Natesan <natesantitan@gmail.com>
2023-12-13 11:22:36 +0530

    postgresql connect to the backend

--- Ecom_B/Ecom_B/settings.py
+++ Ecom_B/Ecom_B/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Ecom_B.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "Ecom",
+        "USER": "Titan",
+        "PASSWORD":"titan004",
+        "HOST":"localhost", 
     }
 }
 
