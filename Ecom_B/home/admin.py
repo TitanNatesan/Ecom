1fbfeec0c7e99e77c40f43431864e101b95fd953
Titan-Natesan <natesantitan@gmail.com>
2023-12-15 15:57:31 +0530

    database for products created

--- Ecom_B/home/admin.py
+++ Ecom_B/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Users, Products
 
-# Register your models here.
+class AdminUser(admin.ModelAdmin):
+    list_display = ("name", 'username', 'number','referalID')
+
+class AdminProduct(admin.ModelAdmin):
+    list_display =('name', 'productID','stock',"sellingPrice",'rating')
+
+admin.site.register(Users,AdminUser)
+admin.site.register(Products,AdminProduct) 
--- Ecom_B/home/models.py
+++ Ecom_B/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 class Users(models.Model):
     name = models.CharField(max_length=100, null= True)
     username = models.CharField(max_length=100)
@@ -7,3 +8,17 @@ class Users(models.Model):
     password = models.CharField(max_length=50)
     referalID = models.CharField(max_length=100+5)
     doj = models.DateTimeField(auto_now_add=True)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    productID = models.CharField()
+    images = models.ImageField(upload_to="media/")
+    mrp = models.DecimalField( max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
+    discount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    sellingPrice = models.IntegerField()
+    stock = models.IntegerField()
+    rating = models.DecimalField(validators=[MaxValueValidator(5),MinValueValidator(0)], max_digits=10,decimal_places= 6)
+    freeDelivery = models.BooleanField(default=True)
+    specification = models.TextField()
